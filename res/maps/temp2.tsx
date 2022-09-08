2fe5ea8056847518c0a81fd9d240ccc0beffcd86
Panda248 <p4nda248@gmail.com>
2022-09-08 09:19:18 -0400

    map manager works

---
+++ res/maps/temp2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="temp2" tilewidth="64" tileheight="64" tilecount="20" columns="5">
+ <image source="../../../../Pictures/gray-background-7131-98db5e6ffc4972baa4087760291cbc3e@1x.jpg" width="360" height="270"/>
+</tileset>
