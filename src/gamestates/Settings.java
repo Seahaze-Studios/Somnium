189141a8987b32233f4ee270440881f0ad01f7f6
LittleWhole <wholelittle@gmail.com>
2022-09-16 08:33:37 -0400

    Settings stuff

--- src/gamestates/Settings.java
+++ src/gamestates/Settings.java
@@ -3,6 +3,8 @@ package gamestates;
 import core.Configuration;
 import core.Main;
 import gamestates.types.AdvancedGameState;
+import graphics.ui.button.Button;
+import graphics.ui.button.PaddedButton;
 import graphics.ui.selection.ToggleBar;
 import graphics.ui.tabber.Tab;
 import graphics.ui.tabber.TabBody;
@@ -15,6 +17,8 @@ import org.newdawn.slick.geom.Line;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.RoundedRectangle;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
 import util.Commandable;
 import util.DrawUtilities;
 import util.bundle.Bundle;
@@ -26,6 +30,7 @@ public class Settings extends AdvancedGameState {
     private RoundedRectangle box;
     private ArrayList<Line> lines;
     private Tabber tabber;
+    private Button saveButton;
     private int counter = 0;
 
     public Settings(int id)
@@ -46,9 +51,10 @@ public class Settings extends AdvancedGameState {
     @Override
     public void enter(GameContainer container, StateBasedGame game) throws SlickException {
         counter = 0;
+        saveButton = new PaddedButton(Main.width() - 50 - 120, Main.height() - 50 - 30, Main.fonts.VariableWidth.P35.getWidth("Save and Exit") + 20, Main.fonts.VariableWidth.P35.getHeight() + 10, "Save and Exit", () -> game.enterState(Main.TITLE_ID, new FadeOutTransition(), new FadeInTransition()));
         tabber = new Tabber(
                 new Tab(
-                        new RoundedRectangle(50, 50, Main.width() / 7 - 50, 50, 20, RoundedRectangle.TOP_LEFT),
+                        new RoundedRectangle(50, 50, Main.width() / 7 - 50, 50, 20, 25, RoundedRectangle.TOP_LEFT),
                         "Graphics",
                         new TabBody(/*new Bundle<String, ToggleBar>("Graphics Quality Preset", new ToggleBar(
                                 new Bundle<String, Commandable>(Configuration.GraphicsQuality.LOW.name(), () -> {}),
@@ -78,14 +84,19 @@ public class Settings extends AdvancedGameState {
     }
 
     @Override
-    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
         counter++;
+        saveButton.update(gc);
     }
 
+    @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
         g.setBackground(Color.black);
         tabber.render(gc);
         g.draw(lines.get(0));
+        saveButton.setX(Main.width() - 50 - 120);
+        saveButton.setY(Main.height() - 50 - 30);
+        saveButton.render(gc);
 //        g.setColor(new Color(255, 255, 255));
 //        g.draw(box);
 //        DrawUtilities.animateLines(g, lines, counter, 1d);
---
+++ src/graphics/ui/button/PaddedButton.java
@@ -0,0 +1,138 @@
+package graphics.ui.button;
+
+import core.Main;
+import gamestates.Game;
+import graphics.ui.UIElement;
+import graphics.ui.menu.Menu;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.RoundedRectangle;
+import util.Commandable;
+import util.DrawUtilities;
+
+public class PaddedButton extends Button implements UIElement {
+    private RoundedRectangle rect;
+
+    public PaddedButton(float width, String text, Commandable command) {
+        super(width, 60, text);
+        this.command = command;
+        rect = new RoundedRectangle(0, 0, width, 60, RoundedRectangle.ALL);
+        DrawUtilities.centerShape(rect, x, y);
+    }
+
+    public PaddedButton(float width, float height, String text, Commandable command) {
+        super(width, height, text);
+        this.command = command;
+        rect = new RoundedRectangle(0, 0, width, height, RoundedRectangle.ALL);
+        DrawUtilities.centerShape(rect, x, y);
+    }
+
+    public PaddedButton(float x, float y, float width, float height, String text, Commandable command) {
+        super(x, y, width, height, text);
+        this.command = command;
+        rect = new RoundedRectangle(x, y, width, height, RoundedRectangle.ALL);
+        DrawUtilities.centerShape(rect, x, y);
+    }
+
+    // Draw the image centered
+    public void render(GameContainer gc) {
+        Graphics g = gc.getGraphics();
+        int mouseX = gc.getInput().getMouseX();
+        int mouseY = gc.getInput().getMouseY();
+        if (onButton(mouseX, mouseY)) {
+            g.setColor(new Color(255, 255, 255));
+            g.fill(rect);
+            g.setColor(Color.black);
+            DrawUtilities.drawStringCentered(g, "> " + text + " <", Main.fonts.VariableWidth.P35, x, y);
+        }
+        else {
+            g.setColor(new Color(180, 180, 180));
+            DrawUtilities.drawStringCentered(g, text, Main.fonts.VariableWidth.P35, x, y);
+        }
+        g.setColor(Color.white);
+    }
+
+    public boolean onButton(int mouseX, int mouseY) {
+        if(x - width / 2 < mouseX && mouseX < x + width / 2) {
+            return y - height / 2 < mouseY && mouseY < y + height / 2;
+        }
+        return false;
+    }
+
+    @Override
+    public void update(GameContainer gc) {
+        if (onButton(gc.getInput().getMouseX(), gc.getInput().getMouseY())) {
+            this.onFrames++;
+            if (gc.getInput().isMousePressed(0)) {
+                //SoundManager.playSoundEffect("click");
+                command.command();
+            }
+        }
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Commandable getCommand() {
+        return command;
+    }
+
+    public Menu getParent() {
+        return parent;
+    }
+
+    public PaddedButton setX(float x) {
+        this.x = x;
+        return this;
+    }
+
+    public PaddedButton setY(float y) {
+        this.y = y;
+        return this;
+    }
+
+    public PaddedButton setWidth(float width) {
+        this.width = width;
+        return this;
+    }
+
+    public PaddedButton setHeight(float height) {
+        this.height = height;
+        return this;
+    }
+
+    public PaddedButton setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public PaddedButton setCommand(Commandable command) {
+        this.command = command;
+        return this;
+    }
+
+    public PaddedButton setParent(Menu parent) {
+        this.parent = parent;
+        return this;
+    }
+}
+
+
