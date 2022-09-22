b3a685ac2ef4451308b925bf24900a34d4a9d403
Panda248 <p4nda248@gmail.com>
2022-09-22 09:14:58 -0400

    worked on hierarchy of tiles

--- src/map/tile/Tile.java
+++ src/map/tile/Tile.java
@@ -1,10 +1,15 @@
 package map.tile;
 
+import core.Constants;
+import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
 
 public abstract class Tile {
     protected Shape hitbox;
 
+    public Tile(int x, int y)  {
+        this.hitbox = new Rectangle(x, y, Constants.TILE_SIZE, Constants.TILE_SIZE);
+    }
     public Shape getHitbox()    {
         return hitbox;
     }
--- src/map/tile/obstacle/Obstacle.java
+++ src/map/tile/obstacle/Obstacle.java
@@ -3,4 +3,8 @@ package map.tile.obstacle;
 import map.tile.Tile;
 
 public abstract class Obstacle extends Tile {
+
+    public Obstacle(int x, int y) {
+        super(x, y);
+    }
 }
